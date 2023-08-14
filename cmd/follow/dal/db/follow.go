a85ac991928b215c8eea4ceefd89a987ca3e94e1
wushiling50 <120616893+wushiling50@users.noreply.github.com>
2023-08-14 20:27:36 +0800

    feat:新增关注操作的数据库部分的函数

--- cmd/follow/dal/db/follow.go
+++ cmd/follow/dal/db/follow.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"context"
+	"errors"
 	"time"
 
 	"gorm.io/gorm"
@@ -14,4 +16,40 @@ type Follow struct {
 	UserId     int64
 	ToUserId   int64
 	ActionType int64 `gorm:"default:1"` //1-关注, 2-取消关注
+}
+
+// func GetUserID(ctx context.Context)(int64,error){
+// 	var id int64
+
+//		err:=DB.WithContext(ctx).
+//	}
+//
+// TODO:判断uid以及touid是否存在与User表中
+func FollowAction(ctx context.Context, follow *Follow) (*Follow, error) {
+	followResp := new(Follow)
+
+	//TODO:redis缓存
+	//若为空，则写入数据库且成功关注
+	err := DB.WithContext(ctx).Model(&Follow{}).
+		Where("user_id= ? AND to_user_id = ?", follow.UserId, follow.ToUserId).
+		First(&followResp).Error
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			if err := DB.WithContext(ctx).Create(follow).Error; err != nil {
+				return nil, err
+			}
+			return follow, nil
+		}
+		return nil, err
+	}
+
+	//关注操作
+	err = DB.WithContext(ctx).Model(&Follow{}).
+		Where("user_id= ? AND to_user_id = ?", follow.UserId, follow.ToUserId).
+		Update("action_type", follow.ActionType).Error
+	if err != nil {
+		return nil, err
+	}
+	return follow, nil
 }
--- cmd/follow/service/action.go
+++ cmd/follow/service/action.go
@@ -1,6 +1,18 @@
 package service
 
-// TODO:关注操作
-// func (s *FollowService) Action(req *follow.ActionRequest) (*db.Follow, error) {
+import (
+	"github.com/ozline/tiktok/cmd/follow/dal/db"
+	"github.com/ozline/tiktok/kitex_gen/follow"
+)
 
-// }
+// Action Functions for the follow/close operation
+func (s *FollowService) Action(req *follow.ActionRequest) (*db.Follow, error) {
+	//TODO:获取用户ID
+	action := &db.Follow{
+		UserId:     10000, //拿到ID再说(先占个位)
+		ToUserId:   req.ToUserId,
+		ActionType: req.ActionType,
+	}
+
+	return db.FollowAction(s.ctx, action)
+}
