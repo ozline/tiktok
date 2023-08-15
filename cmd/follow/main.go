224bd41b430c43f8dda29a1502e749d41e7f02fd
wushiling50 <120616893+wushiling50@users.noreply.github.com>
2023-08-15 20:44:31 +0800

    add: some db funcs related to follow (#32)

--- cmd/follow/main.go
+++ cmd/follow/main.go
@@ -7,13 +7,22 @@ import (
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
 	etcd "github.com/kitex-contrib/registry-etcd"
+	trace "github.com/kitex-contrib/tracer-opentracing"
 	"github.com/ozline/tiktok/cmd/follow/dal"
+	"github.com/ozline/tiktok/cmd/follow/rpc"
 	follow "github.com/ozline/tiktok/kitex_gen/follow/followservice"
 	"github.com/ozline/tiktok/pkg/constants"
+	"github.com/ozline/tiktok/pkg/tracer"
 )
 
-func main() {
+func Init() {
 	dal.Init()
+	rpc.Init()
+	tracer.InitJaeger(constants.FollowServiceName)
+}
+
+func main() {
+	Init()
 
 	r, err := etcd.NewEtcdRegistry([]string{constants.EtcdEndpoints})
 
@@ -32,8 +41,10 @@ func main() {
 		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
 			ServiceName: constants.FollowServiceName,
 		}),
+		server.WithMuxTransport(),
 		server.WithServiceAddr(addr),
 		server.WithRegistry(r),
+		server.WithSuite(trace.NewDefaultServerSuite()),
 		server.WithLimit(&limit.Option{
 			MaxConnections: constants.MaxConnections,
 			MaxQPS:         constants.MaxQPS,
--- cmd/follow/service/action.go
+++ cmd/follow/service/action.go
@@ -1,6 +1,22 @@
 package service
 
-// TODO:关注操作
-// func (s *FollowService) Action(req *follow.ActionRequest) (*db.Follow, error) {
+import (
+	"github.com/ozline/tiktok/cmd/follow/dal/db"
+	"github.com/ozline/tiktok/kitex_gen/follow"
+)
 
-// }
+// Action Function for the follow/close operation
+func (s *FollowService) Action(req *follow.ActionRequest) error {
+	//TODO:获取用户ID
+	// id ,err:= rpc.GetUserId(s.ctx, &user.InfoRequest{
+	// 	UserId: id,
+	// 	Token:  req.Token,
+	// })
+	action := &db.Follow{
+		UserId:     10000, //拿到ID再说(先占个位)
+		ToUserId:   req.ToUserId,
+		ActionType: req.ActionType,
+	}
+
+	return db.FollowAction(s.ctx, action)
+}
