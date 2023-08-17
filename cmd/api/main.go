e991072b42175345ac1cc3d9a914328b606db074
ozline <ozlinex@outlook.com>
2023-08-17 16:22:40 +0800

    add: config_example.yaml for custom config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,11 +11,13 @@ import (
 	"github.com/cloudwego/hertz/pkg/app/middlewares/server/recovery"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/ozline/tiktok/cmd/api/biz/rpc"
 	"github.com/ozline/tiktok/config"
 	"github.com/ozline/tiktok/pkg/constants"
 	"github.com/ozline/tiktok/pkg/errno"
 	"github.com/ozline/tiktok/pkg/tracer"
+	"github.com/ozline/tiktok/pkg/utils"
 )
 
 var (
@@ -27,17 +29,29 @@ func Init() {
 	// config init
 	path = flag.String("config", "./config", "config path")
 	flag.Parse()
-	config.Init(*path, constants.UserServiceName)
+	config.Init(*path, constants.APIServiceName)
 
 	rpc.Init()
-	tracer.InitJaeger(constants.GatewayServiceName)
+	tracer.InitJaeger(constants.APIServiceName)
 }
 
 func main() {
 	Init()
 
+	// get available port from config set
+	for index, addr := range config.Service.AddrList {
+		if ok := utils.AddrCheck(addr); ok {
+			listenAddr = addr
+			break
+		}
+
+		if index == len(config.Service.AddrList)-1 {
+			klog.Fatal("not available port from config")
+		}
+	}
+
 	r := server.New(
-		server.WithHostPorts(constants.GatewayListenAddress),
+		server.WithHostPorts(listenAddr),
 		server.WithHandleMethodNotAllowed(true),
 	)
 
