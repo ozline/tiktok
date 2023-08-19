2af2bce428f346e66d3eb09f023da5c6f1c6010e
jkskj <lllljkskj@163.com>
2023-08-19 10:43:17 +0800

    refactor : comment module

---
+++ test/interaction/favorite_count_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cloudwego/kitex/client/callopt"
+	"github.com/ozline/tiktok/kitex_gen/interaction"
+	"github.com/ozline/tiktok/pkg/constants"
+	"github.com/ozline/tiktok/pkg/errno"
+)
+
+func TestFavoriteCount(t *testing.T) {
+
+	req := &interaction.FavoriteCountRequest{
+		VideoId: "1",
+		Token:   token,
+	}
+
+	resp, err := conn.FavoriteCount(context.Background(), req, callopt.WithRPCTimeout(constants.RPCTimeout))
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	if resp.Base.Code != errno.SuccessCode {
+		t.Error(errno.NewErrNo(resp.Base.Code, *resp.Base.Msg))
+		t.Fail()
+	}
+
+	t.Logf("Resp:\n%v\n\n", resp)
+}
