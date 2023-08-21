2d596df3d5ee0a3b67be31977a99e6e76b6a8ec3
notacommonperson <1932049002@qq.com>
2023-08-21 17:07:59 +0800

    update: logic related to favorite module
    Co-authored-by Ricky <1932049002@qq.com>

--- cmd/interaction/service/get_like_count.go
+++ cmd/interaction/service/get_like_count.go
@@ -1,16 +1,27 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/ozline/tiktok/cmd/interaction/dal/cache"
+	"github.com/ozline/tiktok/cmd/interaction/dal/db"
 	"github.com/ozline/tiktok/kitex_gen/interaction"
+	"github.com/redis/go-redis/v9"
 )
 
 func (s *InteractionService) GetLikeCount(req *interaction.FavoriteCountRequest) (int64, error) {
-
+	// read from redis
 	likeCount, err := cache.GetVideoLikeCount(s.ctx, req.VideoId)
+	if err == redis.Nil {
+		return 0, errors.New("this video has no like")
+	}
 	if err != nil {
 		return 0, err
 	}
+	if likeCount == 0 {
+		// read from mysql
+		likeCount, err = db.GetVideoLikeCount(s.ctx, req.VideoId)
+	}
 
-	return likeCount, nil
+	return likeCount, err
 }
