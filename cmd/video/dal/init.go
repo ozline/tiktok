701948705dda66741327a75197d6bcbcf6d613f2
jiuxia211 <119691804+jiuxia211@users.noreply.github.com>
2023-08-23 01:05:29 +0800

    fix: some issues pointed out by golangci-lint  (#68)
    * fix: redis init and sql
    
    * feat : Increment the return values of favoriteCount and commentCount and Optimize code structure

--- cmd/video/dal/init.go
+++ cmd/video/dal/init.go
@@ -1,7 +1,11 @@
 package dal
 
-import "github.com/ozline/tiktok/cmd/video/dal/db"
+import (
+	"github.com/ozline/tiktok/cmd/interaction/dal/cache"
+	"github.com/ozline/tiktok/cmd/video/dal/db"
+)
 
 func Init() {
 	db.Init()
+	cache.Init()
 }
