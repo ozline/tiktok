9fbc0c460065e46d9cc2afc38cbe8c3dfbab636e
wushiling50 <2531010934@qq.com>
2023-08-22 16:37:05 +0800

    fix:fix some bug

--- cmd/follow/rpc/init.go
+++ cmd/follow/rpc/init.go
@@ -1,9 +1,13 @@
 package rpc
 
-import "github.com/ozline/tiktok/kitex_gen/user/userservice"
+import (
+	"github.com/ozline/tiktok/kitex_gen/chat/messageservice"
+	"github.com/ozline/tiktok/kitex_gen/user/userservice"
+)
 
 var (
 	userClient userservice.Client
+	chatClient messageservice.Client
 )
 
 func Init() {
