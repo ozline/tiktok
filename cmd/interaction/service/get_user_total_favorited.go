1289943d768a5476aa93e0ca4f1162f1fcc66d12
notacommonperson <1932049002@qq.com>
2023-08-25 11:58:38 +0800

    perf: use goroutine concurrent processing writing action

--- cmd/interaction/service/get_user_total_favorited.go
+++ cmd/interaction/service/get_user_total_favorited.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"sync"
+
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/ozline/tiktok/cmd/interaction/dal/cache"
 	"github.com/ozline/tiktok/cmd/interaction/dal/db"
 	"github.com/ozline/tiktok/cmd/interaction/rpc"
@@ -9,7 +12,7 @@ import (
 )
 
 func (s *InteractionService) GetUserTotalFavorited(req *interaction.UserTotalFavoritedRequest) (int64, error) {
-	var total int64
+	var total int64 = 0
 	videoIDList, err := rpc.GetUserVideoList(s.ctx, &video.GetVideoIDByUidRequset{
 		UserId: req.UserId,
 		Token:  req.Token,
@@ -18,14 +21,33 @@ func (s *InteractionService) GetUserTotalFavorited(req *interaction.UserTotalFav
 		return 0, err
 	}
 
+	var wg sync.WaitGroup
+	errChan := make(chan error, len(videoIDList))
 	for _, videoID := range videoIDList {
-		// read from redis
-		likeCount, err := cache.GetVideoLikeCount(s.ctx, videoID)
-		if likeCount == 0 {
-			// read from mysql
-			likeCount, err = db.GetVideoLikeCount(s.ctx, videoID)
-		}
-		total += likeCount
+		wg.Add(1)
+		go func(videoID int64) {
+			// recover panic
+			defer func() {
+				if e := recover(); e != nil {
+					klog.Infof("recover panic :", e)
+				}
+				wg.Done()
+			}()
+			likeCount, err := cache.GetVideoLikeCount(s.ctx, videoID)
+			if likeCount == 0 {
+				// read from mysql
+				likeCount, err = db.GetVideoLikeCount(s.ctx, videoID)
+			}
+			if err != nil {
+				errChan <- err
+			}
+			total += likeCount
+		}(videoID)
+	}
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
 		if err != nil {
 			return total, err
 		}
