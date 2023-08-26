a59da149657a63c00988809203dde0ec8bb203a5
notacommonperson <1932049002@qq.com>
2023-08-26 19:02:34 +0800

    fix: logic bug related to favorite moudule

--- cmd/interaction/service/favorite_list.go
+++ cmd/interaction/service/favorite_list.go
@@ -1,11 +1,14 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/ozline/tiktok/cmd/interaction/dal/cache"
 	"github.com/ozline/tiktok/cmd/interaction/dal/db"
 	"github.com/ozline/tiktok/cmd/interaction/rpc"
 	"github.com/ozline/tiktok/cmd/video/kitex_gen/video"
 	"github.com/ozline/tiktok/kitex_gen/interaction"
+	"gorm.io/gorm"
 )
 
 func (s *InteractionService) FavoriteList(req *interaction.FavoriteListRequest) ([]*video.Video, error) {
@@ -14,18 +17,19 @@ func (s *InteractionService) FavoriteList(req *interaction.FavoriteListRequest) 
 	if err != nil {
 		return nil, err
 	}
-	if len(videoIDList) != 0 {
-		return nil, nil
-	}
-
-	// read from mysql
-	videoIDList, err = db.GetVideosByUserId(s.ctx, req.UserId)
-	if err != nil {
-		return nil, err
-	}
-	err = cache.UpdateFavoriteVideoList(s.ctx, req.UserId, videoIDList)
-	if err != nil {
-		return nil, err
+	if len(videoIDList) == 0 {
+		// read from mysql
+		videoIDList, err = db.GetVideosByUserId(s.ctx, req.UserId)
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		err = cache.UpdateFavoriteVideoList(s.ctx, req.UserId, videoIDList)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return rpc.GetFavoriteVideoList(s.ctx, &video.GetFavoriteVideoInfoRequest{
--- cmd/interaction/service/is_favorite.go
+++ cmd/interaction/service/is_favorite.go
@@ -1,24 +1,30 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/ozline/tiktok/cmd/interaction/dal/cache"
 	"github.com/ozline/tiktok/cmd/interaction/dal/db"
 	"github.com/ozline/tiktok/kitex_gen/interaction"
+	"gorm.io/gorm"
 )
 
 func (s *InteractionService) IsFavorite(req *interaction.IsFavoriteRequest) (bool, error) {
 	// read from redis
 	exist, err := cache.IsVideoLikeExist(s.ctx, req.VideoId, req.UserId)
 	if err != nil {
-		return exist, err
+		return false, err
 	}
 	if exist {
 		return exist, nil
 	}
 	// read from mysql
 	err = db.IsFavoriteExist(s.ctx, req.UserId, req.VideoId)
-	if err != nil {
-		exist = false
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, nil
 	}
-	return exist, err
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
