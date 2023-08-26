3b0ce3210f28ed33abf1ff41a39bbd938a4b6160
Ricky <126139065+notacommonperson@users.noreply.github.com>
2023-08-26 23:49:35 +0800

    fix: favorite logic bug (#90)

--- cmd/interaction/service/favorite_list.go
+++ cmd/interaction/service/favorite_list.go
@@ -1,11 +1,14 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/ozline/tiktok/cmd/interaction/dal/cache"
 	"github.com/ozline/tiktok/cmd/interaction/dal/db"
 	"github.com/ozline/tiktok/cmd/interaction/rpc"
 	"github.com/ozline/tiktok/cmd/video/kitex_gen/video"
 	"github.com/ozline/tiktok/kitex_gen/interaction"
+	"gorm.io/gorm"
 )
 
 func (s *InteractionService) FavoriteList(req *interaction.FavoriteListRequest) ([]*video.Video, error) {
@@ -15,11 +18,17 @@ func (s *InteractionService) FavoriteList(req *interaction.FavoriteListRequest) 
 		return nil, err
 	}
 	if len(videoIDList) != 0 {
-		return nil, nil
+		return rpc.GetFavoriteVideoList(s.ctx, &video.GetFavoriteVideoInfoRequest{
+			VideoId: videoIDList,
+			Token:   req.Token,
+		})
 	}
 
 	// read from mysql
 	videoIDList, err = db.GetVideosByUserId(s.ctx, req.UserId)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -27,7 +36,6 @@ func (s *InteractionService) FavoriteList(req *interaction.FavoriteListRequest) 
 	if err != nil {
 		return nil, err
 	}
-
 	return rpc.GetFavoriteVideoList(s.ctx, &video.GetFavoriteVideoInfoRequest{
 		VideoId: videoIDList,
 		Token:   req.Token,
