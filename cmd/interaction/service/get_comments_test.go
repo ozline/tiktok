10ac641aec8cbf98a09832cb326b44d80b709eaa
jkskj <lllljkskj@163.com>
2023-08-26 14:31:30 +0800

    test: add mock and coverage testing (#85)
    * test: add mock
    
    * test: add coverage testing

---
+++ cmd/interaction/service/get_comments_test.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"bou.ke/monkey"
+	"github.com/ozline/tiktok/cmd/interaction/rpc"
+	"github.com/ozline/tiktok/kitex_gen/interaction"
+	"github.com/ozline/tiktok/kitex_gen/user"
+)
+
+func TestCommentsGet(t *testing.T) {
+	monkey.Patch(rpc.UserInfo, func(ctx context.Context, req *user.InfoRequest) (*user.User, error) {
+		return &user.User{Id: userId}, nil
+	})
+
+	defer monkey.UnpatchAll()
+
+	req1 := &interaction.CommentActionRequest{
+		VideoId:     videoId,
+		CommentText: &commentText,
+		CommentId:   &commentId,
+		Token:       token,
+	}
+
+	_, err := interactionService.CreateComment(req1, userId)
+	if err != nil {
+		t.Logf("err: [%v] \n", err)
+		t.Error(err)
+		t.Fail()
+	}
+	req2 := &interaction.CommentListRequest{
+		VideoId: videoId,
+		Token:   token,
+	}
+
+	_, err = interactionService.GetComments(req2)
+
+	if err != nil {
+		t.Logf("err: [%v] \n", err)
+		t.Error(err)
+		t.Fail()
+	}
+	_, err = interactionService.GetComments(req2)
+
+	if err != nil {
+		t.Logf("err: [%v] \n", err)
+		t.Error(err)
+		t.Fail()
+	}
+}
