494bd1281ef16ce2f178005d9d458f06b1df98d2
jiuxia211 <2064166368@qq.com>
2023-08-27 17:27:08 +0800

    add: coverage testing

---
+++ cmd/video/service/feed_video_test.go
@@ -0,0 +1,46 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/ozline/tiktok/cmd/video/dal"
+	"github.com/ozline/tiktok/cmd/video/kitex_gen/video"
+	"github.com/ozline/tiktok/cmd/video/rpc"
+	"github.com/ozline/tiktok/cmd/video/service"
+	"github.com/ozline/tiktok/config"
+	"github.com/ozline/tiktok/pkg/utils"
+)
+
+func TestFeedVideo(t *testing.T) {
+	config.InitForTest()
+	dal.Init()
+	rpc.Init()
+	token, err := utils.CreateToken(10000)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	videoService := service.NewVideoService(context.Background())
+	// 第一次从数据库读取，并写入redis
+	_, _, _, _, _, err = videoService.FeedVideo(&video.FeedRequest{
+		LatestTime: 1693101739,
+		Token:      token,
+	})
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	// 等待写入redis
+	time.Sleep(time.Second)
+	// 第二次直接从redis读取
+	_, _, _, _, _, err = videoService.FeedVideo(&video.FeedRequest{
+		LatestTime: 1693101739,
+		Token:      token,
+	})
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+}
