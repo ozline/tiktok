5cbcfb283d92a9fdcf0db02ffdd4738d07975b1b
ozline <ozlinex@outlook.com>
2023-08-29 21:51:47 +0800

    ci: update basic-test.yaml

--- test/follow/main_test.go
+++ test/follow/main_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ozline/tiktok/cmd/follow/dal"
+	"github.com/ozline/tiktok/cmd/follow/rpc"
 	"github.com/ozline/tiktok/cmd/follow/service"
 	"github.com/ozline/tiktok/config"
 )
@@ -21,6 +22,7 @@ var (
 func TestMain(m *testing.M) {
 	config.InitForTest()
 	dal.Init()
+	rpc.Init()
 
 	followService = service.NewFollowService(context.Background())
 
