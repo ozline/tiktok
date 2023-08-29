677e7ff7213a0dc503e1f5ee179be5eb56b71d6f
jiuxia211 <119691804+jiuxia211@users.noreply.github.com>
2023-08-30 00:14:47 +0800

    fix: video can be empty and URL splicing bug (#109)
    * fix: update upload err code
    
    * fix: token and latest time can't be empty
    
    * fix: golangci issue
    
    * fix: upload logic err
    
    * feat: improved log
    
    * fix: video bug
    
    * fix: redis does not set an expiration time

--- cmd/video/service/feed_video.go
+++ cmd/video/service/feed_video.go
@@ -26,7 +26,7 @@ func (s *VideoService) FeedVideo(req *video.FeedRequest) ([]db.Video, []*user.Us
 		}
 	} else {
 		formattedTime := time.Unix(*req.LatestTime, 0).Format("2006-01-02 15:04:05")
-		videoList, err := db.GetVideoInfoByTime(s.ctx, formattedTime)
+		videoList, err = db.GetVideoInfoByTime(s.ctx, formattedTime)
 		if err != nil {
 			return nil, nil, nil, nil, nil, err
 		}
