0a588d5fd8d4c234969d699fa4aa41ed14f36357
XZ0730 <94458213+XZ0730@users.noreply.github.com>
2023-08-30 19:35:22 +0800

    Update rpc_test.go

--- test/chat/rpc_test.go
+++ test/chat/rpc_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 func testRpc(t *testing.T) {
-	cli, err := messageservice.NewClient("chat",
+	_, err := messageservice.NewClient("chat",
 		client.WithMuxConnection(constants.MuxConnection),
 		client.WithHostPorts("0.0.0.0:10003"))
 
@@ -20,22 +20,4 @@ func testRpc(t *testing.T) {
 		t.Error(err)
 		t.Fail()
 	}
-	token, err := utils.CreateToken(from_user_id)
-	if err != nil {
-		t.Error(err)
-		t.Fail()
-	}
-	resp, err := cli.MessageList(context.Background(), &chat.MessageListRequest{
-		Token:    token,
-		ToUserId: to_user_id,
-	})
-	if err != nil {
-		t.Logf("result ==> %v", resp)
-		t.Error(err)
-		t.Fail()
-	}
-	t.Logf("result ==> %v", resp.MessageList)
-	t.Logf("result ==> %v", resp.Base)
-	t.Logf("result ==> %v", resp.Total)
-	t.Logf("result ==> %v", len(resp.MessageList))
 }
