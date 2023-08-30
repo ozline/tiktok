1bd1f95734a6873be2a87f36cff256d05ddb4c26
wushiling50 <2531010934@qq.com>
2023-08-30 12:44:14 +0800

    fix: fix some follow bug in test

--- test/follow/action_test.go
+++ test/follow/action_test.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"context"
 	"testing"
 	"time"
 
+	"bou.ke/monkey"
+	"github.com/ozline/tiktok/cmd/follow/rpc"
 	"github.com/ozline/tiktok/kitex_gen/follow"
+	"github.com/ozline/tiktok/kitex_gen/user"
 	"github.com/ozline/tiktok/pkg/utils"
 )
 
 func testAction(t *testing.T) {
+	monkey.Patch(rpc.GetUser, func(ctx context.Context, req *user.InfoRequest) (*user.User, error) {
+		return &user.User{Id: touserid}, nil
+	})
+
+	defer monkey.UnpatchAll()
+
 	token, _ = utils.CreateToken(id)
 	err := followService.Action(&follow.ActionRequest{
 		Token:      token,
