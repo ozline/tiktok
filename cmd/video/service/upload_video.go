318b3fc252a0b1a6c404522bb9386839b5c9af2e
ozline <ozlinex@outlook.com>
2023-08-31 20:36:08 +0800

    optimize: video upload use pipeline

--- cmd/video/service/upload_video.go
+++ cmd/video/service/upload_video.go
@@ -3,11 +3,16 @@ package service
 import (
 	"bytes"
 
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/ozline/tiktok/config"
 	"github.com/ozline/tiktok/kitex_gen/video"
 )
 
 func (s *VideoService) UploadVideo(req *video.PutVideoRequest, videoName string) (err error) {
-	err = s.bucket.PutObject(config.OSS.MainDirectory+"/"+videoName, bytes.NewReader(req.VideoFile))
+	fileReader := bytes.NewReader(req.VideoFile)
+	err = s.bucket.PutObject(config.OSS.MainDirectory+"/"+videoName, fileReader)
+	if err != nil {
+		klog.Errorf("upload file error: %v\n", err)
+	}
 	return
 }
