996fbe76b403a12ca65fbb82dcfc5939ee1bbf51
XZ0730 <1055275260@qq.com>
2023-08-31 19:43:45 +0800

    fix:Update code
    Co-authored-by:XZ0730<1055275260@qq.com>

--- cmd/chat/pack/chat.go
+++ cmd/chat/pack/chat.go
@@ -1,33 +1,29 @@
 package pack
 
 import (
-	"github.com/cloudwego/kitex/pkg/klog"
+	"fmt"
+
 	"github.com/ozline/tiktok/cmd/chat/dal/db"
 	"github.com/ozline/tiktok/kitex_gen/chat"
-	antsGR "github.com/ozline/tiktok/pkg/ants"
 )
 
-type MessageBuild struct {
-	MessageEle  *chat.Message
-	Messagelist []*chat.Message
-}
+type MessageBuildArray []*chat.Message
 
 func BuildMessage(data []*db.Message) []*chat.Message {
 	if data == nil {
 		return make([]*chat.Message, 0)
 	}
-
-	message := &db.MessageBuild{
-		MessageList: make([]*chat.Message, 0),
-	}
+	res := make(MessageBuildArray, 0)
 	for _, val := range data {
-		message.MessageElem = val
-		antsGR.Wg.Add(1)
-		antsGR.AntsPool.Invoke(message)
+		create_at := fmt.Sprintf("%v", val.CreatedAt.UnixMilli())
+		msg := &chat.Message{
+			Id:         val.Id,
+			ToUserId:   val.ToUserId,
+			FromUserId: val.FromUserId,
+			Content:    val.Content,
+			CreateTime: &create_at,
+		}
+		res = append(res, msg)
 	}
-	antsGR.Wg.Wait()
-	for _, v := range message.MessageList {
-		klog.Info("pack build ==> ", *v.CreateTime)
-	}
-	return message.MessageList
+	return res
 }
