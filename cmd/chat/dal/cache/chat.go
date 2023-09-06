1a5bda26b336039b04adcaa0468444c0ec148b6e
Jabbi zhang <94458213+XZ0730@users.noreply.github.com>
2023-09-06 18:40:21 +0800

    Update chat.go

--- cmd/chat/dal/cache/chat.go
+++ cmd/chat/dal/cache/chat.go
@@ -10,7 +10,6 @@ import (
 func MessageInsert(ctx context.Context, key string, revkey string, stamp_key int64, field string) error {
 	// 先判断是否key是否存在，如果存在则判断过期时间是否小于十天，小于则加时间，大于则不加时间
 	if ok := MessageExist(ctx, key); ok != 0 {
-
 		err := RedisDB.HSet(ctx, key, stamp_key, field).Err()
 		if err != nil {
 			klog.Error(err)
