39021ebb3b7874bfff609b5aae0709342b37e132
notacommonperson <1932049002@qq.com>
2023-09-07 21:10:54 +0800

    feat: mq init related to favorite module

--- cmd/interaction/dal/init.go
+++ cmd/interaction/dal/init.go
@@ -3,11 +3,13 @@ package dal
 import (
 	"github.com/ozline/tiktok/cmd/interaction/dal/cache"
 	"github.com/ozline/tiktok/cmd/interaction/dal/db"
+	"github.com/ozline/tiktok/cmd/interaction/dal/mq"
 	"github.com/ozline/tiktok/cmd/interaction/dal/sensitive_words"
 )
 
 func Init(path string) {
 	db.Init()
 	cache.Init()
+	mq.Init()
 	sensitive_words.Init(path)
 }
---
+++ cmd/interaction/dal/mq/init.go
@@ -0,0 +1,55 @@
+package mq
+
+import (
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/ozline/tiktok/pkg/constants"
+	"github.com/ozline/tiktok/pkg/utils"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQ struct {
+	mqURL string
+	conn  *amqp.Connection
+}
+
+type InteractionMQ struct {
+	name    string
+	channel *amqp.Channel
+}
+
+var (
+	mqClient *RabbitMQ
+	LikeMQ   *InteractionMQ
+)
+
+func Init() {
+	mqClient = &RabbitMQ{
+		mqURL: utils.GetMQUrl(),
+	}
+
+	conn, err := amqp.Dial(mqClient.mqURL)
+	if err != nil {
+		klog.Errorf("mq init error: %v", err)
+		panic(err)
+	}
+
+	mqClient.conn = conn
+	LikeMQ, err = NewInteractionMQ(constants.LikeQueueName)
+
+	if err != nil {
+		klog.Errorf("mq init error: %v", err)
+		panic(err)
+	}
+}
+
+func NewInteractionMQ(name string) (*InteractionMQ, error) {
+	channel, err := mqClient.conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	return &InteractionMQ{
+		name:    name,
+		channel: channel,
+	}, nil
+}
