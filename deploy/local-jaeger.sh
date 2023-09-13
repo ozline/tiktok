74370abf38d9768b4d8a1d274ab8dd3b711a7087
XZ0730 <1055275260@qq.com>
2023-09-13 12:48:52 +0800

    fix:local jaeger enviroment import

---
+++ deploy/local-jaeger.sh
@@ -0,0 +1,28 @@
+CONFIGPATH="./config/config.yaml"
+
+function read_key() {
+    local key="$2"
+    local flag=0
+    while read -r LINE; do
+        if [[ $flag == 0 ]]; then
+            if [[ "$LINE" == *"$key:"* ]]; then
+                if [[ "$LINE" == *" "* ]]; then
+                    echo "$LINE" | awk -F " " '{print $2}'
+                    return
+                else
+                    continue
+                fi
+            fi
+        fi
+    done < "$1"
+}
+
+
+# JAEGER
+export JAEGER_DISABLED=false
+export JAEGER_SAMPLER_TYPE="const"
+export JAEGER_SAMPLER_PARAM=1
+export JAEGER_REPORTER_LOG_SPANS=true
+
+export JAEGER_AGENT_HOST=$(read_key $CONFIGPATH "jaeger-host")
+export JAEGER_AGENT_PORT=$(read_key $CONFIGPATH "jaeger-port")
