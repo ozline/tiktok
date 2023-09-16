7aed5986f1bd1cb2d53c2bd5cde81d385da7518f
ozline <ozlinex@outlook.com>
2023-09-17 02:49:42 +0800

    feat: support data persistence & simplify configuration (#145)
    * feat: Support data persistence & simplify configuration
    
    * fix: fix deploy config missing

--- pkg/tracer/tracer.go
+++ pkg/tracer/tracer.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 
 	"github.com/opentracing/opentracing-go"
+	"github.com/ozline/tiktok/config"
 	"github.com/uber/jaeger-client-go"
 	jaegerconfig "github.com/uber/jaeger-client-go/config"
 )
 
 func InitJaeger(service string) {
-	cfg, _ := jaegerconfig.FromEnv()
+	cfg := &jaegerconfig.Configuration{
+		Disabled: false,
+		Sampler: &jaegerconfig.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &jaegerconfig.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: config.Jaeger.Addr,
+		},
+	}
+
 	cfg.ServiceName = service
 
 	tracer, _, err := cfg.NewTracer(
